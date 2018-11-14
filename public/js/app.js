cfa60df041de783f8745e4232872daf62e57a985
Reavershark <jonasty1999@gmail.com>
2018-11-14 17:26:15 +0100

    Implement cards elements

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,24 @@
 let app = new Vue({
     el: '#app',
-    data: {
-        let redCards = new Array(13);
-        let blueCards = new Array(13);
-    },
     methods: {
         updateCards: function() {
             this.$http.get('/teams/cards').then(response => {
-                for (i=0; i>13; i++) {
-                    this.redCards[i].innerHTML = atob(response.body.redCards[i]);
-                    this.blueCards[i].innerHTML = atob(response.body.bluePlayers[i]);
+                for (i=0; i<13; i++) {
+                    if (document.getElementById('redCards').children[i].innerHTML == "") {
+                        document.getElementById('redCards').children[i].innerHTML = atob(response.body.redCards[i]);
+                    }
+                    if (document.getElementById('blueCards').children[i].innerHTML == "") {
+                        document.getElementById('blueCards').children[i].innerHTML = atob(response.body.blueCards[i]);
+                    }
                 }
             });
         }
     },
     mounted: function () {
-        redCards = this.$el.getElementById('redCards').children;
-        blueCards = this.$el.getElementById('blueCards').children;
-
         this.updateCards();
         setInterval(function () {
             this.updateCards();
-        }.bind(this), 500);
+        }.bind(this), 1000);
     }
 
 })
