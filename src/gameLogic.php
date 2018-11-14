3fe20409a326baaff9e50d8c02fd47d14df5ac92
ArneDJ <ArneDeJonghe@protonmail.com>
2018-11-14 20:01:40 +0000

    added cards shuffle

--- src/gameLogic.php
+++ src/gameLogic.php
@@ -1,30 +1,20 @@
 <?php
 
 //variables for testing
-$deck = array(1,2,3,4,5);
-$iteration = 0;
 $playerCards = 0;
 
+$cardNumbers = array('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King');
+$cardColors = array('Spade', 'Hearts', 'Diamonds', 'Clubs');
+$deck = array();
+
+foreach($cardColors as $cardColor)
+{
+	foreach($cardNumbers as $cardNumber)
+	{
+		$deck[] = $cardColor . $cardNumber;
+	}
+}
 
 shuffle($deck);
+var_dump($deck);
 
-function drawCard($deck, $iteration)
-{
-	$drawnCard = $deck[$iteration];
-	$iteration++;
-	return $drawnCard;	
-}
-
-//testing
-$playerCards += drawCard($deck, $iteration);
-echo $playerCards;
-
-
-/*
-function totalPlayerCards()
-{
-	$totalCards = 0;
-	return $totalCards += drawRandomCard($deck);
-}
-
-echo totalPlayerCards();
