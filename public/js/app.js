22fd7e0800b5ad473558ffac4b5bf9dd170d6de5
Reavershark <jonasty1999@gmail.com>
2018-11-14 10:04:40 +0100

    Added playing cards and updated team template

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,27 @@
 let app = new Vue({
-  el: '#app'
+    el: '#app',
+    data: {
+        let redCards = new Array(13);
+        let blueCards = new Array(13);
+    },
+    methods: {
+        updateCards: function() {
+            this.$http.get('/teams/cards').then(response => {
+                for (i=0; i>13; i++) {
+                    this.redCards[i].innerHTML = atob(response.body.redCards[i]);
+                    this.blueCards[i].innerHTML = atob(response.body.bluePlayers[i]);
+                }
+            });
+        }
+    },
+    mounted: function () {
+        redCards = this.$el.getElementById('redCards').children;
+        blueCards = this.$el.getElementById('blueCards').children;
+
+        this.updateCards();
+        setInterval(function () {
+            this.updateCards();
+        }.bind(this), 500);
+    }
+
 })
