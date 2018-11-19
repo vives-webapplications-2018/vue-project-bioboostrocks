daa2897c86d6619b6903f8f9bad62d13a2c3daef
Reavershark <jonasty1999@gmail.com>
2018-11-19 22:26:31 +0100

    Enclose vue in window.onload functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,26 @@
-let app = new Vue({
-    el: '#app',
-    methods: {
-        updateCards: function() {
-            this.$http.get('/teams/cards').then(response => {
-                for (i=0; i<13; i++) {
-                    if (document.getElementById('redCards').children[i].innerHTML == "") {
-                        document.getElementById('redCards').children[i].innerHTML = atob(response.body.redCards[i]);
+window.onload = function() {
+    let app = new Vue({
+        el: '#app',
+        methods: {
+            updateCards: function() {
+                this.$http.get('/teams/cards').then(response => {
+                    for (i=0; i<13; i++) {
+                        if (document.getElementById('redCards').children[i].innerHTML == "") {
+                            document.getElementById('redCards').children[i].innerHTML = atob(response.body.redCards[i]);
+                        }
+                        if (document.getElementById('blueCards').children[i].innerHTML == "") {
+                            document.getElementById('blueCards').children[i].innerHTML = atob(response.body.blueCards[i]);
+                        }
                     }
-                    if (document.getElementById('blueCards').children[i].innerHTML == "") {
-                        document.getElementById('blueCards').children[i].innerHTML = atob(response.body.blueCards[i]);
-                    }
-                }
-            });
-        }
-    },
-    mounted: function () {
-        this.updateCards();
-        setInterval(function () {
+                });
+            }
+        },
+        mounted: function () {
             this.updateCards();
-        }.bind(this), 1000);
-    }
-
-})
+            setInterval(function () {
+                this.updateCards();
+            }.bind(this), 1000);
+        }
+    
+    })
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,23 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        redPlayers: 42,
-        bluePlayers: 21
-    },
-    methods: {
-        getPlayerCount: function () {
-            this.$http.get('/playerCount').then(response => {
-                this.redPlayers = response.body.redPlayers;
-                this.bluePlayers = response.body.bluePlayers;
-            });
-        }
-    },
-    mounted: function () {
-        this.getPlayerCount();
-        setInterval(function () {
+window.onload = function() {
+    let app = new Vue({
+        el: '#app',
+        data: {
+            redPlayers: 42,
+            bluePlayers: 21
+        },
+        methods: {
+            getPlayerCount: function () {
+                this.$http.get('/playerCount').then(response => {
+                    this.redPlayers = response.body.redPlayers;
+                    this.bluePlayers = response.body.bluePlayers;
+                });
+            }
+        },
+        mounted: function () {
             this.getPlayerCount();
-        }.bind(this), 1000);
-    }
-});
+            setInterval(function () {
+                this.getPlayerCount();
+            }.bind(this), 1000);
+        }
+    });
+}
