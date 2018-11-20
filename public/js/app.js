d3cf4f7474eea290205f2e095ac08c2e9d85874b
Reavershark <jonasty1999@gmail.com>
2018-11-20 11:09:36 +0100

    Reduce cards to 11

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ window.onload = function() {
         methods: {
             updateCards: function() {
                 this.$http.get('/teams/cards').then(response => {
-                    for (i=0; i<13; i++) {
+                    for (i=0; i<11; i++) {
                         if (document.getElementById('redCards').children[i].innerHTML == "") {
                             document.getElementById('redCards').children[i].innerHTML = atob(response.body.redCards[i]);
                         }
