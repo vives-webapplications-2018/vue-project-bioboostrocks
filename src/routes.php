21ac97bca6cdbed8c863875631a486fd9bfc1453
Reavershark <jonasty1999@gmail.com>
2018-11-20 11:34:02 +0100

    Work on hit/stand routes

--- src/routes.php
+++ src/routes.php
@@ -28,7 +28,8 @@ $app->get('/teams/cards', function (Request $request, Response $response, array 
             getCardBase64("diamond", "12")
         ),
         "blueCards" => array(
-            getCardBase64("diamond", "12")
+            getCardBase64("diamond", "12"),
+            getCardBase64("diamond", "11")
         )
     );
     return json_encode($array);
@@ -36,18 +37,18 @@ $app->get('/teams/cards', function (Request $request, Response $response, array 
 
 $app->get('/teams/{team}', function (Request $request, Response $response, array $args) {
     $team = $request->getAttribute('team');
-    $this->logger->info("Get '/teams' route");
+    $this->logger->info("Get '/teams/$team' route");
     return $this->renderer->render($response, 'teams.phtml', $args);
 });
 
 $app->get('/teams/{team}/hit', function (Request $request, Response $response, array $args) {
     $team = $request->getAttribute('team');
-    $this->logger->info("Get '/teams' route");
+    $this->logger->info("Get '/teams/$team/stand' route");
     return $this->renderer->render($response, 'teams.phtml', $args);
 });
 
 $app->get('/teams/{team}/stand', function (Request $request, Response $response, array $args) {
     $team = $request->getAttribute('team');
-    $this->logger->info("Get '/teams' route");
+    $this->logger->info("Get '/teams/$team/stand' route");
     return $this->renderer->render($response, 'teams.phtml', $args);
 });
