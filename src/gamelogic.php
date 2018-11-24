ab547a7869cdc3159a0c010164b66368663ce5d9
ArneDJ <arne@ArneDesktop.localdomain>
2018-11-24 22:35:31 +0000

    rewrote getCardBase64 func

--- src/gamelogic.php
+++ src/gamelogic.php
@@ -1,12 +1,14 @@
 <?php
 
 //variables for testing
-$playerCards = 0;
+$redCards = array();
+$blueCards = array();
 
-$cardNumbers = array('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King');
-$cardColors = array('Spade', 'Hearts', 'Diamonds', 'Clubs');
+$cardNumbers = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13');
+$cardColors = array('spade', 'heart', 'diamond', 'club');
 
 $shuffledDeck = shuffleCards($cardColors, $cardNumbers);
+$newCard = hitCard($shuffledDeck);
 
 function shuffleCards($cardColors, $cardNumbers)
 {
@@ -19,18 +21,19 @@ function shuffleCards($cardColors, $cardNumbers)
 	return $deck;
 }
 
-
 function hitCard(&$shuffledDeck)
 {
 	$drawnCard = array_pop($shuffledDeck);
 	return $drawnCard;
 }
 
-
-
-
-function getCardBase64($symbol, $number)
+function getCardBase64($newCard)
 {
+	$number = preg_replace("/[^0-9\.]/", '', $newCard);
+	$symbol = preg_replace('/[0-9]+/', '', $newCard);
+
     $base64 = file_get_contents("cards/$symbol/$symbol$number.html.base64");
     return str_replace("\n", "", $base64);
 }
+
+
